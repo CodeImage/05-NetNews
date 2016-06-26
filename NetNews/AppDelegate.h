c3d2825bce6a0c2f53e4713deb23dc6a74411cd1
codeimage <liujian2010@163.com>
2016-06-26 16:11:58 +0800

    Initial Commit

---
+++ NetNews/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NetNews
+//
+//  Created by JackLou on 6/26/16.
+//  Copyright © 2016 JackLou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
